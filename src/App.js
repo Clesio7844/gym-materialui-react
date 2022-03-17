90b8bb12b33263ee554a720feaf82d2a451c7d06
david <daviddecastro42@live.co.uk>
2022-03-17 07:50:57 +0000

    working on navbar react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './components/about/About';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={() => <div>Home</div>} />
+          <Route exact path='/about' component={() => <div>About</div>} />
+          <Route exact path='/service' component={() => <div>Service</div>} />
+          <Route exact path='/shedule' component={() => <div>Shedule</div>} />
+          <Route exact path='/gallery' component={() => <div>Gallery</div>} />
+          <Route exact path='/blog' component={() => <div>Blog</div>} />
+          <Route exact path='/contact' component={() => <div>Contact</div>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+
 import image from '../../images/logo/logo.png';
 import { makeStyles, Tabs, Tab } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   appbar: {
@@ -13,6 +14,15 @@ const useStyles = makeStyles(theme => ({
     color: 'black',
     boxShadow: '0px 0px 0px 0px',
     padding: '1rem 5rem'
+  },
+  buttonContaine: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
+  },
+  toolbarMargin: {
+    marginBottom: '9em'
   },
   toolbar: {
     display: 'flex',
@@ -71,25 +81,89 @@ const useStyles = makeStyles(theme => ({
 
 const Navbar = () => {
   const classes = useStyles({});
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/about' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/service' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/shedule' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/gallery' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/blog' && value !== 5) {
+      setValue(5);
+    } else if (window.location.pathname === '/contact' && value !== 6) {
+      setValue(6);
+    }
+  }, [value]);
+
   return (
-    <AppBar position='fixed' className={classes.appbar}>
-      <Toolbar className={classes.toolbar}>
-        <IconButton>
-          <img src={image} alt='gym' />
-        </IconButton>
-        <Tabs className={classes.tabs}>
-          <Tab className={classes.tab} label='Home' />
-          <Tab className={classes.tab} label='About' />
-          <Tab className={classes.tab} label='Service' />
-          <Tab className={classes.tab} label='Shedule' />
-          <Tab className={classes.tab} label='Gallery' />
-          <Tab className={classes.tab} label='Blog' />
-        </Tabs>
-        <Button variant='contained' color='secondary' className={classes.btn}>
-          Became a member
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position='fixed' className={classes.appbar}>
+        <Toolbar className={classes.toolbar} disableGutters>
+          <Button
+            component={Link}
+            to='/'
+            disableRipple
+            onClick={() => setValue(0)}
+            className={classes.buttonContaine}
+          >
+            <img src={image} alt='gym' />
+          </Button>
+          <Tabs className={classes.tabs} value={value} onChange={handleChange}>
+            <Tab className={classes.tab} component={Link} to='/' label='Home' />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/about'
+              label='About'
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/service'
+              label='Service'
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/shedule'
+              label='Shedule'
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/gallery'
+              label='Gallery'
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/blog'
+              label='Blog'
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/contact'
+              label='Contact'
+            />
+          </Tabs>
+          <Button variant='contained' color='secondary' className={classes.btn}>
+            Became a member
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 };
 
